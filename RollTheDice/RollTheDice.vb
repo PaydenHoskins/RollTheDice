ce532629bf0e3c3d3867de674f896968f4657088
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-20 08:28:50 -0700

    Rename module and added random gen code

---
+++ RollTheDice/RollTheDice.vb
@@ -0,0 +1,19 @@
+﻿Module RollTheDice
+
+    Sub Main()
+        Dim myLuckyNumber As Integer = 1
+        Randomize(DateAndTime.Now.Millisecond)
+        For i = 1 To 10
+            Console.Write($"{RandomNumber.PadLeft(8)}|")
+        Next
+    End Sub
+
+    Function RandomNumber() As String
+        Dim myRandomNumber As Single
+        Randomize()
+
+        myRandomNumber = Rnd() * 10
+        myRandomNumber = Int(myRandomNumber) + 1
+        Return CInt(myRandomNumber)
+    End Function
+End Module
