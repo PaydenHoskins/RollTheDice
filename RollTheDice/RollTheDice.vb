66e65c3a05e3d82c17da4c3938840808f0b74db6
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-20 08:59:38 -0700

    Random Code

--- RollTheDice/RollTheDice.vb
+++ RollTheDice/RollTheDice.vb
@@ -3,17 +3,32 @@
     Sub Main()
         Dim myLuckyNumber As Integer = 1
         Randomize(DateAndTime.Now.Millisecond)
-        For i = 1 To 10
-            Console.Write($"{RandomNumber.PadLeft(8)}|")
+        For i = 2 To 12
+            TestRandomness()
         Next
     End Sub
 
-    Function RandomNumber() As String
-        Dim myRandomNumber As Single
-        Randomize()
+    Sub TestRandomness()
+        Dim BeanCounter(20) As Integer
+        For i = 1 To 10000
+            BeanCounter(RandomNumber(12, 2)) += 1
 
-        myRandomNumber = Rnd() * 10
-        myRandomNumber = Int(myRandomNumber) + 1
-        Return CInt(myRandomNumber)
+        Next
+
+        For i = LBound(BeanCounter) To UBound(BeanCounter)
+            Console.WriteLine($"{CStr(i).PadLeft(4)} Hit {CStr(BeanCounter(i)).PadLeft(4)} Many times!")
+        Next
+        Console.WriteLine()
+    End Sub
+
+    Function RandomNumber(Max As Integer, min As Integer) As Integer
+        Dim temp As Single
+        Randomize()
+        temp = Rnd()
+        temp *= Max 
+        temp += min
+        'Math.
+        Return CInt(Math.Floor(temp))
     End Function
+
 End Module
