03fb687fe34f0dcd55249c0b43fad94a28776c58
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-23 14:03:03 -0700

    Added code to format it.

--- RollTheDice/RollTheDice.vb
+++ RollTheDice/RollTheDice.vb
@@ -1,34 +1,45 @@
-﻿Module RollTheDice
+﻿'Payden Hoskins
+'RCET2265
+'Spring 2025
+'https://github.com/PaydenHoskins/RollTheDice.git
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+Module RollTheDice
 
     Sub Main()
-        Dim myLuckyNumber As Integer = 1
-        Randomize(DateAndTime.Now.Millisecond)
-        For i = 2 To 12
-            TestRandomness()
-        Next
+        'Call the sub to display the count of the dice
+        DiceCounter()
     End Sub
 
-    Sub TestRandomness()
-        Dim BeanCounter(20) As Integer
-        For i = 1 To 10000
-            BeanCounter(RandomNumber(12, 2)) += 1
-
+    Sub DiceCounter()
+        'Test the randomness of the dice rolls
+        Dim RollCounter(12) As Integer
+        For i = 1 To 1000
+            RollCounter(RandomNumber(1, 12)) += 1
         Next
-
-        For i = LBound(BeanCounter) To UBound(BeanCounter)
-            Console.WriteLine($"{CStr(i).PadLeft(4)} Hit {CStr(BeanCounter(i)).PadLeft(4)} Many times!")
+        Console.WriteLine("Roll of the Dice".PadLeft(45))
+        Console.WriteLine(StrDup(80, "-"))
+        For i = 2 To UBound(RollCounter)
+            Console.Write($"{CStr(i).PadLeft(4)}  |")
+        Next
+        Console.WriteLine()
+        For i = 2 To UBound(RollCounter)
+            Console.Write($"{CStr(RollCounter(i)).PadLeft(4)}  |")
         Next
         Console.WriteLine()
     End Sub
 
-    Function RandomNumber(Max As Integer, min As Integer) As Integer
-        Dim temp As Single
-        Randomize()
-        temp = Rnd()
-        temp *= Max 
-        temp += min
+    Function RandomNumber(Min As Integer, Max As Integer) As Integer
         'Math.
-        Return CInt(Math.Floor(temp))
+        Dim Dice As Single
+        Randomize()
+        Dice = Rnd()
+        Dice *= Max - Min
+        Dice += Min
+        Return CInt(Math.Ceiling(Dice))
     End Function
 
 End Module
